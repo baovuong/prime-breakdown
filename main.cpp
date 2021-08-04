828edd40a6bf056cd151f041dd7f3184480c2482
Bao Vuong <vqbao@lv5.org>
2021-08-04 12:41:08 -0400

    yes

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) {   
     if (argc < 2) {
         cout << argv[0] << " Version " << PrimeBreakdown_VERSION_MAJOR << "."
              << PrimeBreakdown_VERSION_MINOR << endl;
